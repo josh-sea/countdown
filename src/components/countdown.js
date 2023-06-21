88cca93ea2394a889331cb236f3f5a8f85a3bad6
josh-sea <joshua.cocciardi@gmail.com>
2023-06-21 17:06:25 -0400

    added list of events

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -3,17 +3,59 @@ import { Header, Segment } from "semantic-ui-react";
 
 const CountdownTimer = () => {
   const [countdown, setCountdown] = useState("");
+  const [event, setEvent] = useState("");
 
   useEffect(() => {
-    const targetDate = new Date("June 23, 2023 20:00:00 EDT").getTime();
+    const targetDates = [
+      {
+        event: "Joe and Josh's Bromantic Weekend Extravaganza",
+        date: new Date("June 23, 2023 20:00:00 EDT").getTime(),
+      },
+      {
+        event: "Brooks-Cocciardi Fourth of July Cape Cod Adventure",
+        date: new Date("July 1, 2023 8:00:00 EDT").getTime(),
+      },
+      {
+        event: "Dr. Love's & Amelia Rose's Birthday!!!!!",
+        date: new Date("July 25, 2023 8:00:00 EDT").getTime(),
+      },
+      {
+        event: "Josh's Birthday!!!!",
+        date: new Date("August 14, 2023 8:00:00 EDT").getTime(),
+      },
+      {
+        event: "Campbell's Birthday!!!!",
+        date: new Date("August 23, 2023 8:07:00 EDT").getTime(),
+      },
+      {
+        event: "Josh and Ashley's Wedding Anniversary!!!!",
+        date: new Date("August 30, 2023 14:00:00 EDT").getTime(),
+      },
+    ];
+
+    const sortByClosestEvent = (targetDates) => {
+      const currentDate = new Date().getTime();
+
+      targetDates.sort((a, b) => {
+        return a.date - b.date;
+      });
+
+      const closestEvent = targetDates.find((targetDate) => {
+        return targetDate.date - currentDate >= 0;
+      });
+
+      return closestEvent;
+    };
 
     const interval = setInterval(() => {
       const now = new Date().getTime();
+      const getEvent = sortByClosestEvent(targetDates);
+      const targetDate = getEvent.date;
       const distance = targetDate - now;
 
       if (distance < 0) {
         clearInterval(interval);
-        setCountdown("Countdown Complete");
+        setCountdown("Countdown Complete, refresh for the next event!");
       } else {
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor(
@@ -23,6 +65,7 @@ const CountdownTimer = () => {
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+        setEvent(getEvent.event);
       }
     }, 1000);
 
@@ -42,7 +85,7 @@ const CountdownTimer = () => {
       }}
     >
       <Header as="h2" style={{ color: "white" }}>
-        Joe and Josh Weekend Extravagenza
+        {event}
       </Header>
       <Header as="h3" style={{ color: "white" }}>
         {countdown}
